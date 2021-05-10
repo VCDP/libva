1ccb1fe6c9e0d34e1c1ea26b2e6496c5ad13f8f9
Zhizhen Tang <zhizhen.tang@intel.com>
2021-05-10 10:52:41 +0800

    Fix unnecessary duplicate compilation on aic(v2)
    Remove the generating va_version h file from the android mk file
    Used va_version h file that generated at local path
    
    Signed-off-by: Zhizhen Tang <zhizhen.tang@intel.com>

--- va/Android.mk
+++ va/Android.mk
@@ -102,12 +102,6 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(intermediates) \
 	$(LOCAL_C_INCLUDES)
 
-DST := $(intermediates)/va/va_version.h
-SCRIPT := $(LOCAL_PATH)/../build/gen_version.sh
-PARAM1 := $(LOCAL_PATH)/../
-PARAM2 := $(LOCAL_PATH)/va_version.h.in
-$(shell ($(SCRIPT) $(PARAM1) $(PARAM2) > $(DST)))
-
 include $(BUILD_SHARED_LIBRARY)
 
 # For libva-android
